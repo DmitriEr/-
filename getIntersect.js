7bf8b3d1bf999fb5a82c5156446b4ce1877cb854
dmitrii <tomountain@mail.ru>
2024-02-27 08:31:21 +0300

    fixed func

--- getIntersect.js
+++ getIntersect.js
@@ -1,8 +1,16 @@
 const intersect = (nums1, nums2) => {
+  let isNext = true;
   const nums2String = nums2.join('');
 
-  const result = nums1.reduce((prev, item, arr) => {
-    const isInclude = nums2String.includes(Boolean(prev.length) ? `${prev}${item}` : item);
+  const result = nums1.reduce((prev, item, index, arr) => {
+    let isInclude = false;
+
+    if (prev.length) {
+      isInclude = isNext && nums2String.includes(`${prev}${item}`);
+      isNext = isInclude;
+    } else {
+      isInclude = nums2String.includes(item);
+    }
 
     if (isInclude) {
       prev += item;
@@ -16,3 +24,5 @@ const intersect = (nums1, nums2) => {
 
 console.log(intersect([1,2,2,1], [2,2]));
 console.log(intersect([4,9,5], [9,4,9,8,4]));
+console.log(intersect([4,8,5], [9,4,5,8,4]));
+console.log(intersect([3,7,2], [9,4,5,8,4]));
